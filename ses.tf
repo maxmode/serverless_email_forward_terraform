7e8a9a22a1a1e60a636f34d27f16b1b1462c232b
maksymmoskvychev <maksym.moskvychev@emesa.nl>
2018-04-28 18:56:55 +0200

    Removed custom mail_from domain

--- ses.tf
+++ ses.tf
@@ -2,11 +2,6 @@ resource "aws_ses_domain_identity" "email_domain" {
   domain = "${var.domain}"
 }
 
-resource "aws_ses_domain_mail_from" "email_domain" {
-  domain           = "${aws_ses_domain_identity.email_domain.domain}"
-  mail_from_domain = "bounce.${aws_ses_domain_identity.email_domain.domain}"
-}
-
 resource "aws_route53_record" "ses_domain_verification" {
   zone_id = "${var.route53_zone_id}"
   name    = "_amazonses.${var.domain}"
@@ -21,14 +16,6 @@ resource "aws_route53_record" "ses_mx" {
   type = "MX"
   ttl = "1800"
   records = ["10 inbound-smtp.${var.aws_region}.amazonaws.com"]
-}
-
-resource "aws_route53_record" "spf" {
-  zone_id = "${var.route53_zone_id}"
-  name    = "${aws_ses_domain_mail_from.email_domain.mail_from_domain}"
-  type    = "TXT"
-  ttl     = "600"
-  records = ["v=spf1 include:amazonses.com -all"]
 }
 
 resource "aws_ses_receipt_rule" "forward" {
