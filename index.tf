0ba3f9279578f6e49d18e2daaae5dbbcf1f44095
maksymmoskvychev <maksym.moskvychev@emesa.nl>
2018-04-28 19:11:49 +0200

    Removed provider definition

--- index.tf
+++ index.tf
@@ -6,18 +6,8 @@ variable "forward_to" {
   type = "list"
 }
 variable "route53_zone_id" {}
-variable "aws_region" {}
-variable "access_key" {}
-variable "secret_key" {}
 
-provider "aws" {
-  region = "${var.aws_region}"
-  version = "~> 1.12"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-}
 data "aws_caller_identity" "current" {}
-
 provider "archive" {
   version = "~> 1.0"
 }
